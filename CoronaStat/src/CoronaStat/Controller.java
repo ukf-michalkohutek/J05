cbdf387dcbb3b10862be46ffb387ced77da48fda
Lukáš Grofčík <lukas.grofcik@student.ukf.sk>
2020-05-14 11:21:50 +0200

    J05-grofcik-master

--- CoronaStat/src/CoronaStat/Controller.java
+++ CoronaStat/src/CoronaStat/Controller.java
@@ -1,7 +1,10 @@
 package CoronaStat;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -12,24 +15,42 @@ import java.net.http.HttpResponse;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class Controller {
     @FXML private TableView<Result> tableView;
+    @FXML private TextField countryField;
+    @FXML private TextField dateField;
 
     @FXML protected void initialize() {
-        downloadData();
+        setCountries();
+        downloadData("", "");
     }
 
-    static HashMap<String, Result> resultHashMap = new HashMap<>();
+    static HashMap<String, Result> resultHashMap;
+    private HashMap<String, String> countries;
+    static boolean isGlobal;
 
     void addResult(Result res) {
         ObservableList<Result> data = tableView.getItems();
         data.add(res);
     }
 
-    void downloadData() {
+    @FXML void find(ActionEvent event){
+        tableView.getItems().clear();
+        downloadData(countryField.getText(), dateField.getText());
+    }
+
+    void downloadData(String iso, String datum) {
+        iso = transformIso(iso);
+        if(!validateDatum(datum)) datum="";
+        resultHashMap = new HashMap<>();
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://covidapi.info/api/v1/country/SVK")).build();
+        String uri = "https://covidapi.info/api/v1/";
+        if(iso.equals("")) uri += "global"; else uri += "country/"+iso;
+        if(!datum.equals("")) uri += "/"+datum;
+        isGlobal = iso.equals("");
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(uri)).build();
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
@@ -38,6 +59,31 @@ public class Controller {
 
         for (String date : resultHashMap.keySet()) addResult(resultHashMap.get(date));
 
+    }
+
+    void setCountries(){
+        countries = new HashMap<>();
+        for (String iso2 : Locale.getISOCountries(Locale.IsoCountryCode.PART1_ALPHA2)) {
+            Locale l = new Locale("", iso2);
+            String iso3 = l.getISO3Country();
+            countries.put(l.getDisplayCountry(), iso3);
+        }
+    }
+
+    String transformIso(String iso){
+        for(String i:countries.values()) if(iso.equals(i)) return iso;
+        iso = countries.get(iso);
+        if(iso!=null) return iso;
+        return "";
+    }
+
+    boolean validateDatum(String datum){
+        if(datum.length()!=10) return false;
+        for(int i=0;i<=9;i++){
+            if(i==4||i==7) if(datum.charAt(i)!='-') return false;
+            if(i!=4&&i!=7) if(datum.charAt(i)<'0'||datum.charAt(i)>'9') return false;
+        }
+        return true;
     }
 
     private static String jsonParse(String responseString) {
@@ -53,7 +99,8 @@ public class Controller {
         Arrays.sort(resultDatesSorted);
 
         for (String res : resultDatesSorted) {
-            JSONObject result = results.getJSONObject(0).getJSONObject("result").getJSONObject(res);
+            JSONObject result = results.getJSONObject(0).getJSONObject("result");
+            if(!isGlobal) result = result.getJSONObject(res);
 
             String date = res;
             String confirmed = String.valueOf(result.getInt("confirmed"));
@@ -65,6 +112,7 @@ public class Controller {
 
         return null;
     }
+
 }
 
 
