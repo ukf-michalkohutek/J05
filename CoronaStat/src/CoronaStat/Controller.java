2db803b46c917bf045acf55b5eead202ebb31be5
UKF-MiloslavZima <61242489+UKF-MiloslavZima@users.noreply.github.com>
2020-05-20 19:33:09 +0200

    J05 Zima
    Corona

--- CoronaStat/src/CoronaStat/Controller.java
+++ CoronaStat/src/CoronaStat/Controller.java
@@ -1,7 +1,9 @@
 package CoronaStat;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -12,15 +14,25 @@ import java.net.http.HttpResponse;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Locale;
 
 public class Controller {
     @FXML private TableView<Result> tableView;
+    @FXML private TextField countryField;
+    @FXML private TextField monthField;
+    @FXML private TextField dayField;
+    @FXML private Label confirm;
+    private static String answer;
 
     @FXML protected void initialize() {
         downloadData();
     }
 
     static HashMap<String, Result> resultHashMap = new HashMap<>();
+
+    private String[] locales = Locale.getISOCountries();
+    HttpClient client = HttpClient.newHttpClient();
+    HttpRequest request;
 
     void addResult(Result res) {
         ObservableList<Result> data = tableView.getItems();
@@ -65,6 +77,99 @@ public class Controller {
 
         return null;
     }
+
+    private static String countryParse(String responseString) {
+        JSONObject obj = new JSONObject(responseString);
+        JSONArray date = obj.getJSONObject("result").names();
+        JSONObject result = obj.getJSONObject("result").getJSONObject(date.get(0).toString());
+        String confirmed = String.valueOf(result.getInt("confirmed"));
+        String deaths = String.valueOf(result.getInt("deaths"));
+        String recovered = String.valueOf(result.getInt("recovered"));
+        answer = "This Day: " + confirmed + " confirmed, " + deaths + " deaths, " + recovered + " recovered";
+        return null;
+    }
+
+    private static String globalParse(String responseString) {
+        JSONObject obj = new JSONObject(responseString);
+        JSONObject result = obj.getJSONObject("result");
+        String confirmed = String.valueOf(result.getInt("confirmed"));
+        String deaths = String.valueOf(result.getInt("deaths"));
+        String recovered = String.valueOf(result.getInt("recovered"));
+        answer = "Global: " + confirmed + " confirmed, " + deaths + " deaths, " + recovered + " recovered";
+        return null;
+    }
+
+    @FXML
+    protected void setGlobal() {
+        countryField.setText("Global");
+        monthField.setText("");
+        dayField.setText("");
+        confirm.setText("------------------------------------------------------------");
+    }
+
+    @FXML
+    protected void fieldClear() {
+        countryField.setText("");
+        monthField.setText("");
+        dayField.setText("");
+        confirm.setText("------------------------------------------------------------");
+    }
+
+    @FXML
+    protected void updateCountry() {
+        tableView.getItems().clear();
+        resultHashMap.clear();
+        String countryText = "";
+        String monthText = "";
+        String dayText = "";
+        if (countryField.getText().equals("") && monthField.getText().equals("") && dayField.getText().equals("")) {
+            countryField.setText("SVK");
+            //monthField.setText("1");
+            //dayField.setText("22");
+        }
+        if (countryField.getText().equals("Global")) {
+            countryText = "https://covidapi.info/api/v1/global";
+            if (monthField.getText().equals("")) {monthText = "";}
+            else {monthText = monthField.getText();}
+            if (dayField.getText().equals("")) {dayText = "";}
+            else {dayText = dayField.getText();}
+            if (!(monthField.getText().equals("") || dayField.getText().equals(""))) {countryText = countryText + "/2020-" + monthText + "-" + dayText;}
+            request = HttpRequest.newBuilder().uri(URI.create(countryText)).build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(Controller::globalParse)
+                    .join();
+            confirm.setText(answer);
+        }
+        else {
+            for (String countryCode : locales) {
+                Locale locale = new Locale("", countryCode);
+                if ((locale.getDisplayCountry(locale).equals(countryField.getText())) || (locale.getCountry().equals(countryField.getText())) || (locale.getISO3Country().equals(countryField.getText())) ) {
+                    countryText = "https://covidapi.info/api/v1/country/" + locale.getISO3Country();
+                    break;
+                }
+            }
+            request = HttpRequest.newBuilder().uri(URI.create(countryText)).build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(Controller::jsonParse)
+                    .join();
+            for (String date : resultHashMap.keySet()) {addResult(resultHashMap.get(date));}
+            String date[] = java.time.LocalDate.now().toString().split("-");
+            monthText = date[1];
+            dayText = date[2];
+            if (monthField.getText().equals("")) {monthText = "01";}
+            else {monthText = monthField.getText();}
+            if (dayField.getText().equals("")) {dayText = "22";}
+            else {dayText = dayField.getText();}
+            countryText = countryText + "/2020-" + monthText + "-" + dayText;
+            request = HttpRequest.newBuilder().uri(URI.create(countryText)).build();
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(Controller::countryParse)
+                    .join();
+            confirm.setText(answer);
+        }
+
+    }
 }
-
-
--- CoronaStat/src/CoronaStat/Main.java
+++ CoronaStat/src/CoronaStat/Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +14,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("coronaStat.fxml"));
-        primaryStage.setTitle("CoronaStat");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        root.setId("pane");
+        primaryStage.setTitle("Coronavirus Statistics");
+        primaryStage.setResizable(false);
+        Scene scene = new Scene(root, 800, 600);
+        scene.getStylesheets().addAll(this.getClass().getResource("/style.css").toExternalForm());
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
