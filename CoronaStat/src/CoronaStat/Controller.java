90c65e08342d221c3e21913210be8c873bc200f6
ukf-matusklestinec <61313759+ukf-matusklestinec@users.noreply.github.com>
2020-05-21 20:04:24 +0200

    J05
    01100110 01101001 01110110 01100101

--- CoronaStat/src/CoronaStat/Controller.java
+++ CoronaStat/src/CoronaStat/Controller.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -15,9 +17,11 @@ import java.util.HashMap;
 
 public class Controller {
     @FXML private TableView<Result> tableView;
+    @FXML private TextField krajina;
+    @FXML private Text SvetText;
 
     @FXML protected void initialize() {
-        downloadData();
+        stiahnutData();
     }
 
     static HashMap<String, Result> resultHashMap = new HashMap<>();
@@ -27,12 +31,14 @@ public class Controller {
         data.add(res);
     }
 
-    void downloadData() {
+    @FXML public void stiahnutData() {
+        String krajina =  krajina.getText();
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://covidapi.info/api/v1/country/SVK")).build();
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
+
                 .thenApply(Controller::jsonParse)
                 .join();
 
@@ -40,27 +46,49 @@ public class Controller {
 
     }
 
-    private static String jsonParse(String responseString) {
 
 
-        JSONArray results = new JSONArray("[" + responseString + "]");
-        JSONArray resultDates = results.getJSONObject(0).getJSONObject("result").names();
+        static JSONArray globalnastatistika;
+        @FXML public void stiahnutSvetData() {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://covidapi.info/api/v1/global")).build();
 
-        String[] resultDatesSorted = new String[resultDates.length()];
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(Controller::globalJsonParse)
+                    .join();
+        }
 
-        for (int i = 0; i < resultDates.length(); i++) { resultDatesSorted[i] = resultDates.get(i).toString(); }
+        private static String globalJsonParse(String odpoved)  {
+            globalnastatistika = new JSONArray("[" + odpoved + "]" );
+            System.out.println(globalnastatistika);
+            return null;
+        }
+
+    private static String jsonParse(String odpoved) {
+        JSONArray vysledky = new JSONArray("[" + odpoved + "]");
+
+        JSONArray datamvysledkov = vysledky.getJSONObject(0).getJSONObject("výsledky").names();
+
+        String[] resultDatesSorted = new String[datamvysledkov.length()];
+
+        for (int x = 0; x < datamvysledkov.length(); x++) {
+            resultDatesSorted[x] = datamvysledkov.get(x).toString(); }
 
         Arrays.sort(resultDatesSorted);
 
         for (String res : resultDatesSorted) {
-            JSONObject result = results.getJSONObject(0).getJSONObject("result").getJSONObject(res);
+            JSONObject vysledok = vysledky.getJSONObject(0).getJSONObject("vysledok").getJSONObject(res);
 
             String date = res;
-            String confirmed = String.valueOf(result.getInt("confirmed"));
-            String deaths = String.valueOf(result.getInt("deaths"));
-            String recovered = String.valueOf(result.getInt("recovered"));
 
-            resultHashMap.put(res, new Result(res,confirmed,deaths,recovered));
+            String deaths = String.valueOf(vysledok.getInt("Úmrtia"));
+
+            String confirmed = String.valueOf(vysledok.getInt("Potvrdenené"));
+
+            String recovered = String.valueOf(vysledok.getInt("Uzdravenie"));
+
+            resultHashMap.put(res, new Result(res,confirmed,recovered,deaths));
         }
 
         return null;
