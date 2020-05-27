de106e3059115b1bdc53a5c0ce140130be3ebd02
ukf-boristoth <boris.toth@centrum.sk>
2020-05-27 14:21:37 +0200

    J05
    J05

--- CoronaStat/src/CoronaStat/Controller.java
+++ CoronaStat/src/CoronaStat/Controller.java
@@ -1,70 +1,115 @@
 package CoronaStat;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.TreeSet;
 
 public class Controller {
+
+    private static final HashMap<String, Result> results = new HashMap<>();
+    private static String country;
+    private static String date;
+
     @FXML private TableView<Result> tableView;
+    @FXML private DatePicker dateP;
+    @FXML private ComboBox<String> countryC;
+    @FXML private Button downloadBtn;
 
-    @FXML protected void initialize() {
-        downloadData();
+    private static String parseData(String responseString) {
+        try {
+            if (country.equals("GLOBAL") && date != null) {
+                JSONObject obj = new JSONObject(responseString);
+                String date = obj.getString("date");
+                JSONObject day = obj.getJSONObject("result");
+                String confirmed = "" + day.getInt("confirmed");
+                String deaths = "" + day.getInt("deaths");
+                String recovered = "" + day.getInt("recovered");
+                results.put(date, new Result(date, confirmed, deaths, recovered));
+            }
+            else {
+                JSONObject result = new JSONObject(responseString).getJSONObject("result");
+                for (String k : result.keySet()) {
+                    JSONObject day = result.getJSONObject(k);
+                    String confirmed = "" + day.getInt("confirmed");
+                    String deaths = "" + day.getInt("deaths");
+                    String recovered = "" + day.getInt("recovered");
+                    results.put(k, new Result(k, confirmed, deaths, recovered));
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("No info.");
+        }
+        return null;
     }
 
-    static HashMap<String, Result> resultHashMap = new HashMap<>();
+    @FXML
+    protected void initialize() {
+        downloadBtn.setOnAction(e -> download());
+        countryC.getItems().add("GLOBAL");
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("src\\countries.json"));
+            TreeSet<String> countries = new TreeSet<>();
+            String country;
+            while ((country = br.readLine()) != null) {
+                countries.add(country);
+            }
+            br.close();
 
-    void addResult(Result res) {
-        ObservableList<Result> data = tableView.getItems();
-        data.add(res);
+            for (String s : countries) {
+                countryC.getItems().add(s);
+            }
+        } catch (Exception ignored) {
+        }
     }
 
-    void downloadData() {
+    private void download() {
+        date = (dateP.getValue()!=null) ? dateP.getValue().toString() : null;
+        country = countryC.getSelectionModel().getSelectedItem();
+
+        if (date == null) {
+            if (country.equals("GLOBAL")) downloadData("https://covidapi.info/api/v1/global/count");
+            else downloadData("https://covidapi.info/api/v1/country/" + country);
+        } else {
+            if (country.equals("GLOBAL")) downloadData("https://covidapi.info/api/v1/global/" + date);
+            else downloadData("https://covidapi.info/api/v1/country/" + country + "/" + date);
+        }
+        }
+
+
+
+    private void displayData() {
+        tableView.getItems().clear();
+        for (String key : results.keySet()) {
+            tableView.getItems().add(results.get(key));
+        }
+        tableView.getSortOrder().add(tableView.getColumns().get(0));
+
+        dateP.setValue(null);
+    }
+
+    private void downloadData(String requestString) {
+        results.clear();
+
         HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://covidapi.info/api/v1/country/SVK")).build();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(requestString)).build();
 
         client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
-                .thenApply(Controller::jsonParse)
+                .thenApply(Controller::parseData)
                 .join();
 
-        for (String date : resultHashMap.keySet()) addResult(resultHashMap.get(date));
-
-    }
-
-    private static String jsonParse(String responseString) {
-
-
-        JSONArray results = new JSONArray("[" + responseString + "]");
-        JSONArray resultDates = results.getJSONObject(0).getJSONObject("result").names();
-
-        String[] resultDatesSorted = new String[resultDates.length()];
-
-        for (int i = 0; i < resultDates.length(); i++) { resultDatesSorted[i] = resultDates.get(i).toString(); }
-
-        Arrays.sort(resultDatesSorted);
-
-        for (String res : resultDatesSorted) {
-            JSONObject result = results.getJSONObject(0).getJSONObject("result").getJSONObject(res);
-
-            String date = res;
-            String confirmed = String.valueOf(result.getInt("confirmed"));
-            String deaths = String.valueOf(result.getInt("deaths"));
-            String recovered = String.valueOf(result.getInt("recovered"));
-
-            resultHashMap.put(res, new Result(res,confirmed,deaths,recovered));
-        }
-
-        return null;
+        displayData();
     }
 }
-
-
