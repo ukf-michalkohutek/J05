facd13653c46f70d597aedd4631b447261db9c3a
programatorroka <lukasprokop547@gmail.com>
2020-07-10 10:15:16 +0200

    zmeny

--- CoronaStat/src/CoronaStat/Controller.java
+++ CoronaStat/src/CoronaStat/Controller.java
@@ -1,7 +1,8 @@
 package CoronaStat;
-import javafx.collections.ObservableList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -9,60 +10,105 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Controller {
-    @FXML private TableView<Result> tableView;
-
-    @FXML protected void initialize() {
-        downloadData();
-    }
+    @FXML
+    private TableView<Result> tableView;
+    @FXML
+    private TextField country;
+    @FXML
+    private TextField month;
+    @FXML
+    private TextField day;
+    @FXML
 
     static HashMap<String, Result> resultHashMap = new HashMap<>();
 
-    void addResult(Result res) {
-        ObservableList<Result> data = tableView.getItems();
-        data.add(res);
+    @FXML
+    protected void initialize() {
+        downloadData();
     }
 
-    void downloadData() {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://covidapi.info/api/v1/country/SVK")).build();
+    @FXML
+    protected void downloadData() {
+        tableView.getItems().clear();
+        resultHashMap.clear();
+        boolean divnyFormat = false;
 
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+        String path = "https://covidapi.info/api/v1/";
+
+        if (country.getText().equalsIgnoreCase("")) {
+            //format: {"count":169,"result":{"2020-01-22":{"confirmed":0,"deaths":0,"recovered":0},"2020-01-23":{"confirmed":0,"deaths":0,"recovered":0},
+            path += "global";
+            if (month.getText().equals("") || day.getText().equals("")) { //ak nie je vyplneny datum
+                path += "/count";
+            } else {
+                // format: "count":1,"date":"2020-07-08","result":{"confirmed":12041480,"deaths":549468,"recovered":6586726}
+                divnyFormat = true;
+                if (!month.getText().equals("") && !day.getText().equals("")) {
+                    path += "/2020-" + month.getText() + "-" + day.getText();
+                }
+            }
+        } else {
+            path += "country/" + country.getText().toUpperCase(); //aby vzdy bolo velkymi
+            if (!month.getText().equals("") && !day.getText().equals("")) {
+                path += "/2020-" + month.getText() + "-" + day.getText();
+            }
+        }
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(path)).build();
+
+        if (divnyFormat)
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenApply(Controller::jsonParseDivny)
+                    .join();
+        else client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
                 .thenApply(HttpResponse::body)
                 .thenApply(Controller::jsonParse)
                 .join();
 
-        for (String date : resultHashMap.keySet()) addResult(resultHashMap.get(date));
-
+        for (String date : resultHashMap.keySet()) tableView.getItems().add(resultHashMap.get(date));
     }
 
     private static String jsonParse(String responseString) {
+        JSONObject result = new JSONObject(responseString);
+        JSONArray results = result.getJSONObject("result").names();
 
+        String[] resultDatesSorted = new String[results.length()];
 
-        JSONArray results = new JSONArray("[" + responseString + "]");
-        JSONArray resultDates = results.getJSONObject(0).getJSONObject("result").names();
-
-        String[] resultDatesSorted = new String[resultDates.length()];
-
-        for (int i = 0; i < resultDates.length(); i++) { resultDatesSorted[i] = resultDates.get(i).toString(); }
+        for (int i = 0; i < results.length(); i++) {
+            resultDatesSorted[i] = results.get(i).toString();
+        }
 
         Arrays.sort(resultDatesSorted);
 
-        for (String res : resultDatesSorted) {
-            JSONObject result = results.getJSONObject(0).getJSONObject("result").getJSONObject(res);
+        for (String date : resultDatesSorted) {
+            JSONObject resultOnDate = result.getJSONObject("result").getJSONObject(date);
 
-            String date = res;
-            String confirmed = String.valueOf(result.getInt("confirmed"));
-            String deaths = String.valueOf(result.getInt("deaths"));
-            String recovered = String.valueOf(result.getInt("recovered"));
+            String confirmed = String.valueOf(resultOnDate.getInt("confirmed"));
+            String deaths = String.valueOf(resultOnDate.getInt("deaths"));
+            String recovered = String.valueOf(resultOnDate.getInt("recovered"));
 
-            resultHashMap.put(res, new Result(res,confirmed,deaths,recovered));
+            resultHashMap.put(date, new Result(date, confirmed, deaths, recovered));
         }
+        return null;
+    }
 
+    private static String jsonParseDivny(String responseString) {
+        JSONObject result = new JSONObject(responseString);
+        JSONObject resultOnDate = result.getJSONObject("result");
+
+        String date = result.getString("date");
+        String confirmed = String.valueOf(resultOnDate.getInt("confirmed"));
+        String deaths = String.valueOf(resultOnDate.getInt("deaths"));
+        String recovered = String.valueOf(resultOnDate.getInt("recovered"));
+
+        resultHashMap.put(date, new Result(date, confirmed, deaths, recovered));
         return null;
     }
 }
